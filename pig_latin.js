d11cd88c03d89460daa47a4fee625537db3d6454
pisanggoreng <7ofpentacles@gmail.com>
2017-02-08 11:50:16 +0700

    clear

--- pig_latin.js
+++ pig_latin.js
@@ -23,4 +23,60 @@ let convert = (sentence) => {
   return result.join(" ")
 }
 
+function pig_latin(kata) {
+  var vowel = ["a","i","u","e","o"],
+      pisah = kata.split(""),
+      tampung = "",
+      tampung_vowel = "",
+      tampung_non_vowel = "";
+  if (kata[0] != "a" && kata[0] != "i" && kata[0] != "u" && kata[0] != "e" && kata[0] != "o") {
+    tampung_non_vowel += "-";
+  }
+
+  while (pisah.length > 0) {
+    // panjang kata & isi kata
+    // jika kata pertama adalah huruf vokal
+    // ini sudah clear
+    if (pisah[0] == "a" || pisah[0] == "i" || pisah[0] == "u" || pisah[0] == "e" || pisah[0] == "o") {
+      tampung = pisah.join("");
+      pisah = [];
+      // rekursif selesai!!!!!!!!
+    }
+    // jika kata pertama, bukan huruf vokal
+    else {
+      tampung_non_vowel += pisah[0];
+
+    }
+    pisah.shift();
+  }
+  // rekursif berakhir
+
+  if (kata[0] != "a" && kata[0] != "i" && kata[0] != "u" && kata[0] != "e" && kata[0] != "o") {
+    tampung_non_vowel += "ay";
+  }
+  return tampung +  tampung_non_vowel;
+}
+
+function complete_stence(kata) {
+  var result = [];
+  if ( kata.split(" ").length > 1 ) {
+    var arr = kata.split(" ");
+    // kode 2 kata masuk disini
+    for (var i = 0; i < arr.length; i++) {
+      result.push(pig_latin(arr[i]));
+    }
+    return result;
+
+  } else {
+    console.log(pig_latin(kata));;
+  }
+}
+
 // Your CLI code here
+var tampung = process.argv;
+// buang index ke 0 & 1
+tampung.shift();
+tampung.shift();
+// gabung isi array menjadi string
+var tes = tampung.join(" ");
+console.log( complete_stence(tes) );
